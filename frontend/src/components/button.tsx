2b8e9b7a86c3ec0d291313b5f515f4ae04dee1a3
euanlimzx <euanlimzx@gmail.com>
2025-03-01 15:38:00 -0800

    scroll to top done

--- frontend/src/components/button.tsx
+++ frontend/src/components/button.tsx
@@ -1,3 +1,10 @@
 export default function Button() {
-  return <button className="bg-white text-black">Click me</button>;
+  return (
+    <button
+      className="bg-white text-black"
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    >
+      Click me
+    </button>
+  );
 }
