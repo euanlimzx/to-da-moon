5031146e16017afb12f3617502e2b4f63a0d0f28
euanlimzx <euanlimzx@gmail.com>
2025-03-01 17:05:52 -0800

    staggered rocket scroll

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import Background from "@/components/background";
 import Dashboard from "@/components/dashboard";
+import Rocket from "@/components/rocket";
 import { backgroundHeight } from "@/constants";
+import { calculatePixelHeight } from "@/utils";
+import { useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,19 +18,28 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const [rocketHeight, setRocketHeight] = useState(0);
+
   //This is an example of how we can dynamically render the page height based on the current position of the rocket
   //we need to scale based on the max altitude we predict the rocket will achieve
   //we should probably not shift the screen down to avoid jank
   //we probably want to sample values so we don't spam our scroll function
   function buttonFn() {
-    const arr = [0.95, 0.9, 0.7, 0.3, 0.2, 0.1];
+    const arr = [0.1, 0.2, 0.3, 0.7, 0.9];
     arr.forEach((value, index) => {
-      const height = value * backgroundHeight * document.body.scrollHeight;
+      const height =
+        (1 - value) * backgroundHeight * document.body.scrollHeight;
       setTimeout(() => {
         window.scrollTo({
           top: height,
           behavior: "smooth",
         });
+        console.log(value);
+        if (value <= 0.45) {
+          setRocketHeight(calculatePixelHeight(value));
+        } else {
+          setRocketHeight(calculatePixelHeight(0.45));
+        }
       }, index * 400); // Adding a delay between scrolls, won't be visible / won't work otherwise
     });
   }
@@ -35,6 +47,7 @@ export default function Home() {
   return (
     <>
       <Background />
+      <Rocket rocketHeight={rocketHeight} />
       <div className="h-screen w-screen flex justify-center items-center">
         <Dashboard buttonFn={buttonFn} />
       </div>
