249993fb3e5afb5f74382dc083ee321efce1b56b
euanlimzx <euanlimzx@gmail.com>
2025-03-31 13:21:15 -0700

    updated env code

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,2 +1,5 @@
 import io from 'socket.io-client'
-export const socket = io('http://localhost:3000', { autoConnect: true })
+export const socket = io(
+    process.env.NEXT_PUBLIC_WEBSOCKET_DOMAIN || 'http://localhost:3000',
+    { autoConnect: true }
+)
