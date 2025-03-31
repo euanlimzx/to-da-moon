44dd7addc777d36abd9b8815a223cee2a50463b4
euanlimzx <euanlimzx@gmail.com>
2025-03-31 12:50:19 -0700

    added recharts

--- frontend/src/pages/static/[slug].tsx
+++ frontend/src/pages/static/[slug].tsx
@@ -4,9 +4,20 @@ import { socket } from '../../socket'
 import Dashboard from '@/components/dashboard'
 import Button from '@/components/button'
 import { Gauge } from '@/components/gauge'
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+} from 'recharts'
 
 export default function Page() {
     const [values, setValues] = useState<string[]>([])
+    const [data, setData] = useState([])
 
     const router = useRouter()
 
@@ -15,7 +26,13 @@ export default function Page() {
         // Set up the socket listener to receive the data stream
         socket.on('static/receive-data-stream', (message: string[]) => {
             setValues(message) // Update state with the latest values
-            console.log(message)
+            console.log(parseInt(message[0]))
+            setData((prevData) => {
+                const newData = [...prevData, parseInt(message[0])]
+                console.log(newData) // Log the updated data array
+                return newData
+            })
+            console.log(data)
         })
 
         // Clean up the socket listener on component unmount
@@ -34,6 +51,22 @@ export default function Page() {
         <Dashboard>
             <>
                 <Button buttonFn={buttonFn} />
+                <div style={{ width: '100%', height: 300 }}>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <LineChart data={data}>
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="time" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line
+                                type="monotone"
+                                dataKey="value"
+                                stroke="white"
+                            />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
                 {values.map((value) => {
                     return (
                         <Gauge
