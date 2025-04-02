4c551d5deb419392e7596a892e4b35a5492ab2ed
SamuelLo <samuello300@gmail.com>
2025-04-01 19:15:16 -0700

    linearProgressBar shows up in dashboard

--- frontend/src/components/dashboard/index.tsx
+++ frontend/src/components/dashboard/index.tsx
@@ -1,9 +1,13 @@
 import { ReactElement } from 'react'
 import Object from '@/components/rocket-object'
+import LinearProgress from '@/components/dashboard/linearProgress'
 export default function Dashboard({ children }: { children: ReactElement }) {
     return (
         <div className="fixed">
             <Object />
+            <LinearProgress
+                percent={50}
+            />
             {children}
         </div>
     )
