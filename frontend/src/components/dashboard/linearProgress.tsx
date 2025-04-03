f3ec733aa023d40758367c4f2f64a5713b11b885
euanlimzx <euanlimzx@gmail.com>
2025-04-02 20:39:43 -0700

    changed progress trail color

--- frontend/src/components/dashboard/linearProgress.tsx
+++ frontend/src/components/dashboard/linearProgress.tsx
@@ -4,27 +4,29 @@ import type { ProgressProps } from 'antd/es/progress'
 
 interface LinearProgressProps {
     percent: number
-    trailColor ?: string
+    trailColor?: string
 }
 
 const twoColors: ProgressProps['strokeColor'] = {
     '60%': '#507CFF',
-    '100%': "#C0D7FA"
-};
+    '100%': '#C0D7FA',
+}
 
-
-const LinearProgress = ( {percent, trailColor = "#808080"} : LinearProgressProps) => {
-  return (
-    <Flex gap="small" vertical>
-        <Progress
-            percent={percent}
-            percentPosition={{ align: 'end', type: 'inner' }}
-            size={[400, 20]}
-            strokeColor={twoColors}
-            trailColor={trailColor}
-        />
-    </Flex>
-  )
+const LinearProgress = ({
+    percent,
+    trailColor = 'rgba(0, 0, 0, 0.5)',
+}: LinearProgressProps) => {
+    return (
+        <Flex gap="small" vertical>
+            <Progress
+                percent={percent}
+                percentPosition={{ align: 'end', type: 'inner' }}
+                size={[400, 20]}
+                strokeColor={twoColors}
+                trailColor={trailColor}
+            />
+        </Flex>
+    )
 }
 
 export default LinearProgress
