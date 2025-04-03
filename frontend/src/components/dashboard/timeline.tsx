d812ca13c4840ee74e382100c8527d173338777d
euanlimzx <euanlimzx@gmail.com>
2025-04-02 22:31:07 -0700

    timeline UI done

--- frontend/src/components/dashboard/timeline.tsx
+++ frontend/src/components/dashboard/timeline.tsx
@@ -3,10 +3,17 @@ import LinearProgress from './linearProgress'
 
 const Timeline = () => {
     const numberOfStages = 5
-    const [progress, setProgress] = useState(2)
+    const [progress, setProgress] = useState(3)
+    const [isActive, setIsActive] = useState(false)
 
     return (
         <div>
+            <div className="flex items-center gap-2 pb-2">
+                <div
+                    className={`flex h-4 w-4 items-center justify-center rounded-full ${isActive ? 'bg-red-500' : 'bg-green-500'}`}
+                ></div>
+                {isActive ? 'LIVE' : 'INACTIVE'}
+            </div>
             <LinearProgress percent={(progress / numberOfStages) * 100} />
             <div className="flex w-full items-center justify-between">
                 {/* Dynamically generate circles */}
