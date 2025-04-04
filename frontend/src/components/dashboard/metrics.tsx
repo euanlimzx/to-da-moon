66c675c3d335d018dc5bdcb6261c35d9b9f5c7b8
SamuelLo <samuello300@gmail.com>
2025-04-04 01:16:19 -0700

    dropdown works in a static page but does not work on page with 3JS animation

--- frontend/src/components/dashboard/metrics.tsx
+++ frontend/src/components/dashboard/metrics.tsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import DarkCard from "./darkCard";
 import { ChartLine } from "lucide-react";
 import LinearProgress from "./linearProgress";
 import { Gauge } from "../gauge";
 import React from 'react'
+import MetricProgressBar from "./metricProgressBar";
+
+
 
 /*
+Dropdown: 
+  - should be able to change props of the progress bar
+  - therefore, can use state for unit
+  - unit changed on update of dropdown
+  - should be able to pass an array of values to dropdown
+
+create seperate component for the metricsProgressBar and the dropdown so dropdown 
+can provide context to the metricsProgressBar
 want to make it easy to take in props to change percent of progress Bars
 */
 interface MetricsProps {
@@ -13,46 +25,32 @@ interface MetricsProps {
     pressure3: number
     gaugePressure : number
 }
-interface MetricProgressBarProps {
-    value: number;
-    unitType?: string;
-}
-
-// Turn metricProgressBar into a React component
-const MetricProgressBar: React.FC<MetricProgressBarProps> = ({
-    value,
-    unitType = "psi",
-  }) => {
-    return (
-      <div className="col w-full gap-2"
-            style={{ width: "15vw" }} // Set width to 10% of the viewport width
-      >
-        <p className="text-md">{unitType} Progress</p>
-        <div className="flex items-center space-x-2">
-          <LinearProgress percent={value} strokeWidth={10} />
-          <p className="text-sm">
-            {value} {unitType}
-          </p>
-        </div>
-      </div>
-    );
-  };
-
-
-function graphIcon() {
-    return (
-        <ChartLine/>
-    )
-}
 
 
 const Metrics = ({pressure1, pressure2, pressure3, gaugePressure} : MetricsProps) => {
+ 
+    
+
+  function graphIcon() {
+    return (
+        <ChartLine/>
+    )
+  }
   return (
     <div className="">
         <DarkCard header="SENSORS" headerIcon={graphIcon()}>
-            <MetricProgressBar value={pressure1} unitType="psi" />
-            <MetricProgressBar value={pressure2} unitType="psi" />
-            <MetricProgressBar value={pressure3} unitType="psi" />
+            <MetricProgressBar
+                value={pressure1}
+                unitType="psi"
+            />
+            <MetricProgressBar
+                value={pressure2}
+                unitType="degrees"
+            />
+            <MetricProgressBar
+                value={pressure3}
+                unitType="miles"
+            />
             <Gauge 
                 value={gaugePressure}
                 size={'medium'}
