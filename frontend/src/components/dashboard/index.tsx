3c36afe06ed2160e1e9608a0364de9ea874f5d9a
euanlimzx <euanlimzx@gmail.com>
2025-04-04 22:15:53 -0700

    feat: live overview updates

--- frontend/src/components/dashboard/index.tsx
+++ frontend/src/components/dashboard/index.tsx
@@ -1,18 +1,14 @@
-import { useState, ReactElement } from 'react'
 import Timeline from './timeline'
-import Overview from './overview'
-export default function Dashboard() {
-    const numStages = 5
-    const [currStage, setCurrStage] = useState(3)
-    const [isActive, setIsActive] = useState(true)
+import Overview, { Config } from './overview'
+export default function Dashboard({ config }: { config: Config }) {
     return (
         <div className="absolute z-50 h-screen w-screen p-10">
             <Timeline
-                numStages={numStages}
-                currStage={currStage}
-                isActive={isActive}
+                numStages={config.numStages}
+                currStage={config.currStage}
+                isActive={config.isActive}
             />
-            <Overview />
+            <Overview config={config} />
         </div>
     )
 }
