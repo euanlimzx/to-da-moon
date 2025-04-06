6cc7ff7c636b04e0b49d78a285cd92770b329a10
SleepyWoodpecker <shawnweichew@gmail.com>
2025-04-05 21:47:13 -0700

    Modified dark card based on feedback
    Note that there is no smooth way to dynamically specify padding with p-${padding}, since Tailwind will only explicitly generate code for
    classes that exist in your code, hence the hacky fix

--- frontend/src/components/dashboard/darkCard.tsx
+++ frontend/src/components/dashboard/darkCard.tsx
@@ -4,22 +4,35 @@ interface DarkCardProps {
     children: ReactNode
     header?: string
     headerIcon?: ReactNode
-    lessPadding?: boolean
-    grow?: boolean
+    padding?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 8 | 10 | 12 | 16
     hideOverflow?: boolean
+}
+
+// Map of padding values to Tailwind classes
+const paddingClasses = {
+    0: 'p-0',
+    1: 'p-1',
+    2: 'p-2',
+    3: 'p-3',
+    4: 'p-4',
+    5: 'p-5',
+    6: 'p-6',
+    8: 'p-8',
+    10: 'p-10',
+    12: 'p-12',
+    16: 'p-16',
 }
 
 export default function DarkCard({
     children,
     header,
     headerIcon,
-    lessPadding = false,
-    grow = false,
+    padding = 5,
     hideOverflow = false,
 }: DarkCardProps) {
     return (
         <div
-            className={`z-10 inline-block rounded-lg bg-black bg-opacity-50 shadow-lg ${grow ? 'h-full w-full' : ''} ${lessPadding ? 'p-3' : 'p-5'} ${hideOverflow ? 'overflow-hidden' : ''}`}
+            className={`z-10 inline-block h-full w-full rounded-lg bg-black bg-opacity-50 shadow-lg ${paddingClasses[padding]} ${hideOverflow ? 'overflow-hidden' : ''}`}
         >
             {(header || headerIcon) && (
                 <div className="mb-4 flex items-center text-white">
@@ -29,7 +42,7 @@ export default function DarkCard({
                     )}
                 </div>
             )}
-            <div className={`${grow ? 'h-full w-full' : ''}`}>{children}</div>
+            <div className="h-full w-full">{children}</div>
         </div>
     )
 }
